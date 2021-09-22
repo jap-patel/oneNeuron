ebf16d1f24e6bb9f428e7beac02634cb654cd25f
jap <jappatel1704@gmail.com>
2021-09-22 10:51:02 +0530

    final commit

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -71,4 +71,3 @@ def save_plot(df, file_name, model):
   os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
   plotPath = os.path.join(plot_dir, file_name) # model/filename
   plt.savefig(plotPath)
-
